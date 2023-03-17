43dfd4dc89edd504b850205a19183e3f368a1aca
Shoham <medinashoham@gmail.com>
2023-03-17 11:36:09 -0600

    se crea proyecto zapato

---
+++ src/Testsneakers.java
@@ -0,0 +1,13 @@
+public class Testsneakers {
+        public static void main(String[] args) {
+            Sneakers zapatos = new Sneakers();
+            zapatos.setModel(5678910);
+            zapatos.setSize(6.5);
+            zapatos.setColor("negros");
+            zapatos.setMarca("adidas");
+
+
+
+            zapatos.toString();
+        }
+}
